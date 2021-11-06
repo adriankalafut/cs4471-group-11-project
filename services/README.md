e1dcd73d9153490d9506fc5ac835c32be85718c8
Kevin Cox <kevin.cox.work@gmail.com>
2021-11-06 09:25:47 -0400

    added example route for query parameters. Confirmed operation on AWS

--- services/README.md
+++ services/README.md
@@ -18,7 +18,9 @@ Steps To Promote & Expose A Service on AWS
 Assure port is exposed in Dockerfile
 Build and push to ECR
 Define new Task in ECS
-Push Service to Cluster in ECS (Assure that you enable DNS lookup)
+Update/Push Service to Cluster in ECS (Assure that you enable DNS lookup)
+Note: if you are updating an existing service, you will likely have to scale the number of tasks to 0, then back to 1 for the chance to take effect.
+
 Define Route In API Gateway
 Within "Parameter Mapping" of the route, map the corresponding route name which exists in flask
 ```
--- services/live_visualization_service/service_main.py
+++ services/live_visualization_service/service_main.py
@@ -2,6 +2,7 @@ import flask
 import rds_config
 import pymysql
 import json
+from flask import request
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -12,7 +13,6 @@ db_name = rds_config.db_name
 
 @app.route('/example_route_get_btc', methods=['GET'])
 def example_route_get_btc():
-    
     try:
         conn = pymysql.connect(host=rds_config.rds_host, user=name,
                            passwd=password, db=db_name, connect_timeout=5, cursorclass=pymysql.cursors.DictCursor)
@@ -25,4 +25,26 @@ def example_route_get_btc():
         query_result = cur.fetchall()
         return str(query_result)
     return str({"Error": "Bad Query"})
+
+@app.route('/example_get_coin_query_parameters', methods=['GET'])
+def example_get_coin_query_parameters():
+    try:
+        conn = pymysql.connect(host=rds_config.rds_host, user=name,
+                           passwd=password, db=db_name, connect_timeout=5, cursorclass=pymysql.cursors.DictCursor)
+    except pymysql.MySQLError as e:
+        # This should be an actual error
+        return str({"Error": "Can't Connect To DB" })
+
+    symbol = request.args.get('symbol', "")
+    symbol.strip()
+    symbol = conn.escape_string(symbol)
+    
+    # This is hardcore SQL injection, ignore for now, just wanna test query parameters
+    with conn.cursor() as cur:
+        cur.execute(f"SELECT * FROM coin_data.coin_data WHERE Symbol=\"{symbol}\" LIMIT 10;")
+        query_result = cur.fetchall()
+        return str(query_result)
+    return str({"Error": "Bad Query"})
+
+
 app.run(debug=True, host='0.0.0.0')
