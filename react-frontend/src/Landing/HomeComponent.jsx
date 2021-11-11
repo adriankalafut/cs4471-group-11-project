f2bbe96a29a64e9bf5c6806308be230056ad9e87
kevinscode <kevin.cox.work@gmail.com>
2021-11-11 08:30:51 -0500

    improved landing page

--- react-frontend/src/Landing/HomeComponent.jsx
+++ react-frontend/src/Landing/HomeComponent.jsx
@@ -1,16 +1,35 @@
 import React from "react";
 import styled from "styled-components";
+import landing_art from '../static/landing_art_long.png'
 
 const CenteredDiv = styled.div`
-  text-align: center;
-  vertical-align: middle;
+  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;  /* Centering y-axis */
+  align-items :center;
 `;
+
+const SizedImage = styled.img`
+  width: 100vw;
+`
+const StyledTitle = styled.h1`
+  padding-top: 10vh;
+  margin-bottom: 0;
+  font-size: 3em;
+`
+
+const StyledSubTitle = styled.h1`
+  font-style: italic;
+`
 
 export default function HomeComponent() {
   return (
     <CenteredDiv>
-      <h1>CS4471 - Stock Tracker Homepage</h1>
-      <p>I'll clean this up later</p>
+      <StyledTitle>CS4471 - Cryptocurrency Tracker</StyledTitle>
+      <StyledSubTitle>A Cryptocurrency Analysis Tool</StyledSubTitle>
+      <p>Jingtian Chen, Teran Bukenberger, Adrian Kalafut, Ashley Shu, Kevin Cox</p>
+      <SizedImage src={landing_art} alt="Landing Page Art"/>
     </CenteredDiv>
   );
 }
